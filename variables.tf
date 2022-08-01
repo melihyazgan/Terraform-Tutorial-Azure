ba1c658c8ca957f13b1d00119e6d5eb93b0102d5
meya <yazganmelih@outlook.com>
2022-08-01 15:33:42 +0200

    Update variables.tf
    Azure specific infos deleted

--- variables.tf
+++ variables.tf
@@ -11,22 +11,22 @@ variable "rsgname" {
 
 variable "ARM_CLIENT_ID" {
   type    = string
-  default = "e0a0b9a6-7559-48a1-b1cc-e7567b84f02e"
+  default = ""
 }
 
 variable "ARM_CLIENT_SECRET" {
   type    = string
-  default = "zwm8Q~YwOCw0csna5PUBf20zhR7LEeRRwLKWIauw"
+  default = ""
 }
 
 variable "ARM_SUBSCRIPTION_ID" {
   type    = string
-  default = "ffd96b6f-1fcb-4bb8-b0ad-b151d795064a"
+  default = ""
 }
 
 variable "ARM_TENANT_ID" {
   type    = string
-  default = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4"
+  default = ""
 }
 
 variable "ML_Service" {
